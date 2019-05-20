3a2438e4fa139523473e463712819cd4753013ac
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2019-05-20 10:36:03 +0300

    frontend 404

--- src/main/frontend/src/app/404/404Page.js
+++ src/main/frontend/src/app/404/404Page.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import icon404 from "../../images/icons/404.png"
+
 
 export default class Page404 extends Component {
 
     render() {
         const {location} = this.props;
         return (
-            <div>Ничего не знаю о "{location.pathname}" у меня лапки...</div>
+            <div className="">
+                <img className="ui middle aligned medium image" alt={"картинка"} src={icon404}/>
+                <span>Ничего не знаю о "{location.pathname}" у меня лапки...</span>
+            </div>
         )
     }
 }
