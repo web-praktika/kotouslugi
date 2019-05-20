9023b1c61c5725713469011cef9df1e2862d8b4e
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2019-05-20 10:38:10 +0300

    frontend 404

--- src/main/frontend/src/app/404/404Page.js
+++ src/main/frontend/src/app/404/404Page.js
@@ -8,8 +8,8 @@ export default class Page404 extends Component {
         const {location} = this.props;
         return (
             <div className="">
-                <img className="ui middle aligned medium image" alt={"картинка"} src={icon404}/>
-                <span>Ничего не знаю о "{location.pathname}" у меня лапки...</span>
+                <img className="ui centered medium image" alt={"картинка"} src={icon404}/>
+                <h3 className="ui center aligned icon header">Ничего не знаю о "{location.pathname}" у меня лапки...</h3>
             </div>
         )
     }
