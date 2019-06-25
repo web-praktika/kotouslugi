71353c2347136ac898ea71982e094a2c8d2d3b2c
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2019-06-25 10:15:44 +0300

    День первый

--- src/main/frontend/src/app/App.js
+++ src/main/frontend/src/app/App.js
@@ -1,49 +1,11 @@
 import React, {Component} from 'react';
 import {hot} from 'react-hot-loader/root'
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import CatalogPage from "./Catalog/СatalogPage";
-import Page404 from "./404/404Page";
-import TestServicePage from "./TestService/FormPage";
-import Header from "./Header";
-import Breadcrumb from "./Breadcrumb";
-import Footer from "./Footer";
 import "./style.css"
-import 'semantic-ui-css/semantic.min.css'
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import OrdersListPage from "./OrdersList/OrdersListPage";
 
 class App extends Component {
 
-    state ={
-        BreadcrumbLabel :  'Каталог услуг'
-    };
-
-    changeBreadcrumbLabel = (label) =>{
-        this.setState({BreadcrumbLabel: label})
-    };
-
     render() {
-
-        return (
-            <div className="mainWrapper">
-                <Header/>
-                <div className="ui divider"/>
-                <Breadcrumb BreadcrumbLabel={this.state.BreadcrumbLabel}/>
-                <div className="ui divider"/>
-                <main className="ui container ">
-                    <Router>
-                        <Switch>
-                            <Route path="/" exact component={CatalogPage}/>
-                            <Route path="/1" exact render={() => (<TestServicePage changeBreadcrumbLabel={this.changeBreadcrumbLabel}/>)}/>
-                            <Route path="/orders" exact render={() => (<OrdersListPage changeBreadcrumbLabel={this.changeBreadcrumbLabel}/>)}/>
-
-                            <Route component={Page404}/>
-                        </Switch>
-                    </Router>
-                </main>
-                <Footer/>
-            </div>
-        )
+        return <div>Тест</div>
     }
 }
 
