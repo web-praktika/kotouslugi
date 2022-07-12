66fa723d17a17cae779d82977675a12e3858a543
Ptitsyn Nikita <283820@niuitmo.ru>
2022-07-12 21:24:33 +0300

    Правки бэка от Никиты

--- src/main/java/ru/praktika/kotouslugi/controller/LicenceRequisitionController.java
+++ src/main/java/ru/praktika/kotouslugi/controller/LicenceRequisitionController.java
@@ -3,12 +3,11 @@ package ru.praktika.kotouslugi.controller;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.praktika.kotouslugi.exception.ServiceException;
 import ru.praktika.kotouslugi.model.LicenceRequisition;
-import ru.praktika.kotouslugi.model.response.BaseResponse;
 import ru.praktika.kotouslugi.service.LicenceRequisitionService;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/licenceRequisition")
@@ -19,33 +18,17 @@ public class LicenceRequisitionController extends BaseController {
     private LicenceRequisitionService licenceRequisitionService;
 
     @RequestMapping(value = "listLicenceRequisition", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK", response = BaseResponse.class),
-            @ApiResponse(code = 401, message = "Не авторизованный пользователь"),
-            @ApiResponse(code = 500, message = "Внутренняя ошибка") })
-    public BaseResponse<List<LicenceRequisition>> listLicenceRequisition() {
-        return wrapper((s) -> licenceRequisitionService.listLicenceRequisition());
+    public List<LicenceRequisition> listLicenceRequisition() {
+        return licenceRequisitionService.listLicenceRequisition();
     }
 
     @RequestMapping(value = "createLicenceRequisition", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK", response = BaseResponse.class),
-            @ApiResponse(code = 401, message = "Не авторизованный пользователь"),
-            @ApiResponse(code = 500, message = "Внутренняя ошибка") })
-    public BaseResponse<Long> createLicenceRequisition(@ApiParam(name = "request", value = "Тело запроса", required = true)
-                                                   @RequestBody LicenceRequisition licenceRequisition) {
-        return wrapper((s) -> licenceRequisitionService.createLicenceRequisition(licenceRequisition));
+    public Long createLicenceRequisition(@RequestBody LicenceRequisition licenceRequisition) {
+        return licenceRequisitionService.createLicenceRequisition(licenceRequisition);
     }
 
     @RequestMapping(value = "updateLicenceRequisition", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK", response = BaseResponse.class),
-            @ApiResponse(code = 401, message = "Не авторизованный пользователь"),
-            @ApiResponse(code = 500, message = "Внутренняя ошибка") })
-    public BaseResponse<Boolean> updateLicenceRequisition(@RequestBody LicenceRequisition licenceRequisition) {
-        return wrapper((s) -> licenceRequisitionService.updateLicenceRequisition(licenceRequisition));
+    public Boolean updateLicenceRequisition(@RequestBody LicenceRequisition licenceRequisition) throws ServiceException {
+        return licenceRequisitionService.updateLicenceRequisition(licenceRequisition);
     }
 }
