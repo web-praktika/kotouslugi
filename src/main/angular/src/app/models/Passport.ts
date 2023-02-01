367a7aca45e04da5e58d0205cf92c96cb4e07f44
vectorg3 <diglytail@gmail.com>
2023-02-01 18:28:00 +0300

    service_updated

--- src/main/angular/src/app/models/Passport.ts
+++ src/main/angular/src/app/models/Passport.ts
@@ -1,8 +1,12 @@
 export interface Passport {
-    id?: number;
-    name: string;
-    breed: string;
-    cat_COLOR: string;
-    sex: string;
-    photo: string;
-  }
+  id?: number;
+  name: string;
+  breed: string;
+  cat_COLOR: string;
+  sex: string;
+  photo: string;
+  father: string;
+  mother: string;
+  reason_for_replacement: string;
+  marital_status: string;
+}
