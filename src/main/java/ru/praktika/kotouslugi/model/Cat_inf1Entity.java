f657dc1edec0551e78898a061bc4685612a57984
sashaCHEgrincev <sasha_chegrincev@mail.ru>
2023-02-01 17:10:59 +0300

    бек(изменение структуры бд)

--- src/main/java/ru/praktika/kotouslugi/model/Cat_inf1Entity.java
+++ src/main/java/ru/praktika/kotouslugi/model/Cat_inf1Entity.java
@@ -8,23 +8,28 @@ import javax.persistence.*;
 public class Cat_inf1Entity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "CAT_ID", nullable = false)
-    private Integer CAT_ID;
+    @Column(name = "ID", nullable = false)
+    private Integer ID;
     private String NAME;
     private String SEX;
     private String BREED;
     private String CAT_COLOR;
     private String PHOTO;
+    private String REASON_FOR_REPLACEMENT;
+    private String MARITAL_STATUS;
+    private String FATHER;
+    private String MOTHER;
+
 
     public Cat_inf1Entity() {
 
     }
 
     public Integer getId() {
-        return CAT_ID;
+        return ID;
     }
-    public void setId(Integer CAT_ID) {
-        this.CAT_ID = CAT_ID;
+    public void setId(Integer ID) {
+        this.ID = ID;
     }
 
     public String getNAME() {
@@ -51,5 +56,31 @@ public class Cat_inf1Entity {
 
     public String getPHOTO() { return PHOTO; }
     public void setPHOTO(String PHOTO) { this.PHOTO = PHOTO; }
+
+    public String getREASON_FOR_REPLACEMENT() {
+        return REASON_FOR_REPLACEMENT;
+    }
+    public void setREASON_FOR_REPLACEMENT(String REASON_FOR_REPLACEMENT) { this.REASON_FOR_REPLACEMENT = REASON_FOR_REPLACEMENT; }
+
+    public String getMARITAL_STATUS() {
+        return MARITAL_STATUS;
+    }
+    public void setMARITAL_STATUS(String MARITAL_STATUS) {
+        this.MARITAL_STATUS = MARITAL_STATUS;
+    }
+
+    public String getFATHER() {
+        return FATHER;
+    }
+    public void setFATHER(String FATHER) {
+        this.FATHER = FATHER;
+    }
+
+    public String getMOTHER() {
+        return MOTHER;
+    }
+    public void setMOTHER(String MOTHER) {
+        this.MOTHER = MOTHER;
+    }
 
 }
