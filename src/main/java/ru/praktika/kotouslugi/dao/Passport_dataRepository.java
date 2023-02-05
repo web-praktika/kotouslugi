a9512e46773f0437c6407aadba995f42f339dc7c
vectorg3 <diglytail@gmail.com>
2023-02-05 18:52:32 +0300

    files_delete

--- src/main/java/ru/praktika/kotouslugi/dao/Passport_dataRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.praktika.kotouslugi.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.praktika.kotouslugi.model.Passport_dataEntity;
-
-public interface Passport_dataRepository extends CrudRepository<Passport_dataEntity, Integer>{
-}
