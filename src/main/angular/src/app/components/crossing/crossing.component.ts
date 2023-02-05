33c6eb82eb641d3dffcb72f61f0986b35db4fd4b
Konyshenn <konyshenn-ryslan@mail.ru>
2023-02-05 15:09:28 +0300

    Апгрейд вида

--- src/main/angular/src/app/components/crossing/crossing.component.ts
+++ src/main/angular/src/app/components/crossing/crossing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-crossing',
@@ -7,10 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrossingComponent implements OnInit {
 
+    public petForm: FormGroup;
+    public crossingForm: FormGroup;
+    public data: any;
+    public readonly steps = [{
+      id: 1,
+      icon: '/assets/svg/paw.svg',
+      title: 'Информация о пациенте',
+      description: 'Укажите необходимую информацию о пациенте'
+    }, {
+      id: 2,
+      icon: '/assets/svg/hospital.svg',
+      title: 'Выбор специалиста',
+      description: 'Укажите специалиста и время приема'
+    }, {
+      id: 3,
+      icon: '/assets/svg/tasks.svg',
+      title: 'Подтверждение заявки',
+      description: 'Проверьте корректность заполнения заявки'
+    }];
+    public step = 1;
 
-  constructor() { }
+
+  constructor(
+
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.petForm = this.fb.group({
+      HumanName: '',
+      HumanAge: '',
+      number: '',
+      mail: ''
+    });
+ this.crossingForm = this.fb.group({
+      name: '',
+      lastName: '',
+      sex: 'male',
+      age: '',
+      weight: '',
+      color: ''
+    });
+  }
+   public next(): void {
+    switch (this.step) {
+      case 1:
+        this.step++;
+        break;
+      case 2:
+        this.step++;
+        this.data = {
+          ...this.petForm.getRawValue(),
+          ...this.crossingForm.getRawValue()
+        };
+        break;
+
+    }
   }
 
+  public prev(): void {
+    this.step--;
+  }
 }
