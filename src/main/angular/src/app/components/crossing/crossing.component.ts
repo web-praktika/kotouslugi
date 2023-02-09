a2962acdb55bb3a81e3ece3c1fac2c282e3273d0
Konyshenn <konyshenn-ryslan@mail.ru>
2023-02-09 14:01:44 +0300

    The end?!

--- src/main/angular/src/app/components/crossing/crossing.component.ts
+++ src/main/angular/src/app/components/crossing/crossing.component.ts
@@ -48,7 +48,7 @@ export class CrossingComponent implements OnInit {
 
   ngOnInit(): void {
     this.humanForm = this.fb.group({
-      HumanName: new FormControl('', [Validators.required, Validators.pattern(/^[А-яЁё]+[' ']+[А-яЁё]/)]),
+      HumanName: new FormControl('', [Validators.required, Validators.pattern(/^[А-яЁё]+[' ']+[А-яЁё]+[' ']+[А-яЁё]+$/)]),
       HumanAge: new FormControl('', [Validators.max(122), Validators.min(18),Validators.required]),
       number: ['', [Validators.required, Validators.pattern("^[0-9_-]{10,12}")]],
       mail:['', Validators.email]
